4c341b0556232924d909142d81ed728bb52cc704
Prashant Varanasi <prashant@prashantv.com>
2016-10-24 08:45:30 -0700

    Add interface conversion benchmark

---
+++ iface_allocs_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+var _ifaceLast interface{}
+
+type emptyStruct struct{}
+
+type nonemptyStruct struct{ Name string }
+
+func BenchmarkIFaceAllocs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ifaceLast = emptyStruct{}
+	}
+}
+
+func BenchmarkEmptyStructPtrInterface(b *testing.B) {
+	s := &emptyStruct{}
+	for i := 0; i < b.N; i++ {
+		_ifaceLast = s
+	}
+}
+
+func BenchmarkSmallStructInterface(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ifaceLast = nonemptyStruct{}
+	}
+}
+
+func BenchmarkSmallStructPtrInterface(b *testing.B) {
+	s := &nonemptyStruct{}
+	for i := 0; i < b.N; i++ {
+		_ifaceLast = s
+	}
+}
